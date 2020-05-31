4fa9e701b0e9f1142cf8b6ef60136b096e31f1c6
Henry Tom <henry.tom.sf@gmail.com>
2020-05-30 23:35:26 -0700

    added implementation of simulatePlan

--- src/main/scala/retcalc.scala
+++ src/main/scala/retcalc.scala
@@ -19,6 +19,21 @@ object RetCalc {
                      netIncome: Int,
                      currentExpenses: Int,
                      initialCapital: Double): (Double, Double) = {
-        
+        val capitalAtRetirement = futureCapital(
+            interestRate = interestRate,
+            nbOfMonths = nbOfMonthsSaving,
+            netIncome = netIncome,
+            currentExpenses = currentExpenses,
+            initialCapital = initialCapital
+        )
+
+        val capitalAfterDeath = futureCapital(
+            interestRate = interestRate,
+            nbOfMonths = nbOfMonthsInRetirement,
+            netIncome = netIncome,
+            currentExpenses = currentExpenses,
+            initialCapital = initialCapital
+        )
+        (capitalAtRetirement, capitalAfterDeath)
     }
 }
