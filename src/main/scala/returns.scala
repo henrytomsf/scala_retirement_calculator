437e701eebed30bdb61d1915a1271519634a0ca0
Henry Tom <henry.tom.sf@gmail.com>
2020-05-31 00:47:01 -0700

    added implementation of fromUntil in trait

--- src/main/scala/returns.scala
+++ src/main/scala/returns.scala
@@ -4,4 +4,10 @@ package retcalc
 sealed trait Returns
 case class FixedReturns(annualRate: Double) extends Returns
 case class VariableReturn(monthId: String, monthlyRate: Double)
-case class VariableReturns(returns: Vector[VariableReturn]) extends Returns
+case class VariableReturns(returns: Vector[VariableReturn]) extends Returns {
+    def fromUntil(monthIdFrom: String, monthIdUntil: String): VariableReturns = {
+        VariableReturns(
+            returns.dropWhile(_.monthId != monthIdFrom).takeWhile(_.monthId != monthIdUntil)
+        )
+    }
+}
