8825e6bf2e62dead399bc9ccc180969d26b17f76
Henry Tom <henry.tom.sf@gmail.com>
2020-05-30 23:29:39 -0700

    changed function formats

--- src/main/scala/retcalc.scala
+++ src/main/scala/retcalc.scala
@@ -1,11 +1,24 @@
 package retcalc
 
 object RetCalc {
-    def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int, currentExpenses: Int, initialCapital: Double): Double = {
+    def futureCapital(interestRate: Double,
+                      nbOfMonths: Int,
+                      netIncome: Int,
+                      currentExpenses: Int,
+                      initialCapital: Double): Double = {
         val monthlySavings = netIncome - currentExpenses
         def nextCapital(accumulated: Double, month: Int): Double = {
             accumulated*(1+interestRate) + monthlySavings
         }
         (0 until nbOfMonths).foldLeft(initialCapital)(nextCapital)
+    }
+
+    def simulatePlan(interestRate: Double,
+                     nbOfMonthsSaving: Int,
+                     nbOfMonthsInRetirement: Int,
+                     netIncome: Int,
+                     currentExpenses: Int,
+                     initialCapital: Double): Double = {
+        
     }
 }
