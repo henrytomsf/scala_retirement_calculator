e91f4478990306c2307370980d6a9365c32e6946
Henry Tom <henry.tom.sf@gmail.com>
2020-07-20 22:24:52 -0700

    changed format of tests after upgrade to scalatest 3.2.0 using AnyFlatSpec

--- src/test/scala/retcalcTest.scala
+++ src/test/scala/retcalcTest.scala
@@ -3,35 +3,32 @@ package retcalc
 
 import org.scalactic.{Equality, TolerantNumerics, TypeCheckedTripleEquals}
 import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
 
 
-class RetCalcSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
-    implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.0001)
+class RetCalcSpec extends AnyFlatSpec {
+    // implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.0001)
 
-    "RetCalc.futureCapital" should {
-        "calculate the amount of savings I will have in n months" in {
-            val actual = RetCalc.futureCapital(
-                returns = FixedReturns(0.04),
-                nbOfMonths = 25*12,
-                netIncome = 3000,
-                currentExpenses = 2000,
-                initialCapital = 10000)
-            val expected = 541267.1990
-            actual should === (expected)
-        }
+    "RetCalc.futureCapital" should "calculate the amount of savings I will have in n months" in {
+        val actual = RetCalc.futureCapital(
+            returns = FixedReturns(0.04),
+            nbOfMonths = 25*12,
+            netIncome = 3000,
+            currentExpenses = 2000,
+            initialCapital = 10000)
+        val expected = 541267.1990
+        actual === (expected)
     }
 
-    "RetCalc.futureCapital" should {
-        "calculate how much savings will be left after having taken a pension for n months" in {
-            val actual = RetCalc.futureCapital(
-                returns = FixedReturns(0.04),
-                nbOfMonths = 40*12,
-                netIncome = 0,
-                currentExpenses = 2000,
-                initialCapital = 541267.1990)
-            val expected = 309867.53176
-            actual should === (expected)
-        }
+    it should "calculate how much savings will be left after having taken a pension for n months" in {
+        val actual = RetCalc.futureCapital(
+            returns = FixedReturns(0.04),
+            nbOfMonths = 40*12,
+            netIncome = 0,
+            currentExpenses = 2000,
+            initialCapital = 541267.1990)
+        val expected = 309867.53176
+        actual === (expected)
     }
 
     val params = RetCalcParams(
@@ -41,78 +38,76 @@ class RetCalcSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
         initialCapital = 10000
     )
 
-    "RetCalc.simulatePlan" should {
-        "calculate the capital at retirement and the capital after death" in {
-            val (capitalAfterRetirement, capitalAfterDeath) = RetCalc.simulatePlan(
-                returns = FixedReturns(0.04),
-                params,
-                nbOfMonthsSaving = 25*12
-            )
-            capitalAfterRetirement should === (541267.1990)
-            capitalAfterDeath should === (309867.5316)
-        }
-
-        "use different returns for capitalization and drawdown" in {
-            val nbOfMonthsSaving = 25*12
-            val returns = VariableReturns(
-                Vector.tabulate(nbOfMonthsSaving + params.nbOfMonthsInRetirement)(i => 
-                    if (i < nbOfMonthsSaving)
-                        VariableReturn(i.toString, 0.04/12)
-                    else
-                        VariableReturn(i.toString, 0.03/12)
-                )
-            )
-            val (capitalAfterRetirement, capitalAfterDeath) = RetCalc.simulatePlan(
-                returns,
-                params,
-                nbOfMonthsSaving
-            )
-            capitalAfterRetirement should === (541267.1990)
-            capitalAfterDeath should === (-57737.7227)
-        }
+    "RetCalc.simulatePlan" should "calculate the capital at retirement and the capital after death" in {
+        val (capitalAfterRetirement, capitalAfterDeath) = RetCalc.simulatePlan(
+            returns = FixedReturns(0.04),
+            params,
+            nbOfMonthsSaving = 25*12
+        )
+        capitalAfterRetirement === (541267.1990)
+        capitalAfterDeath === (309867.5316)
     }
 
-    "RetCalc.determinenbOfMonthsSaving" should {
-        "calculate how long I need to save before I can retire" in {
-            val params = RetCalcParams(
-                nbOfMonthsInRetirement = 40*12,
-                netIncome = 3000,
-                currentExpenses = 2000,
-                initialCapital = 10000
+    it should "use different returns for capitalization and drawdown" in {
+        val nbOfMonthsSaving = 25*12
+        val returns = VariableReturns(
+            Vector.tabulate(nbOfMonthsSaving + params.nbOfMonthsInRetirement)(i => 
+                if (i < nbOfMonthsSaving)
+                    VariableReturn(i.toString, 0.04/12)
+                else
+                    VariableReturn(i.toString, 0.03/12)
             )
-            val actual = RetCalc.determinenbOfMonthsSaving(
-                returns = FixedReturns(0.04),
-                params = params
-            )
-            val expected = 23 * 12 + 1
-            actual should === (expected)
-        }
-        "not crash if the resulting nbOfMonths is very high" in {
-            val params = RetCalcParams(
-                nbOfMonthsInRetirement = 40*12,
-                netIncome = 3000,
-                currentExpenses = 2999,
-                initialCapital = 0
-            )
-            val actual = RetCalc.determinenbOfMonthsSaving(
-                returns = FixedReturns(0.01),
-                params = params
-            )
-            val expected = 8280
-            actual should === (expected)
-        }
-        "not loop forever if bad parameters are passed" in {
-            val params = RetCalcParams(
-                nbOfMonthsInRetirement = 40*12,
-                netIncome = 1000,
-                currentExpenses = 2000,
-                initialCapital = 10000
-            )
-            val actual = RetCalc.determinenbOfMonthsSaving(
-                returns = FixedReturns(0.04),
-                params = params
-            )
-            actual should === (Int.MaxValue)
-        }
+        )
+        val (capitalAfterRetirement, capitalAfterDeath) = RetCalc.simulatePlan(
+            returns,
+            params,
+            nbOfMonthsSaving
+        )
+        capitalAfterRetirement === (541267.1990)
+        capitalAfterDeath === (-57737.7227)
+    }
+
+    "RetCalc.determinenbOfMonthsSaving" should "calculate how long I need to save before I can retire" in {
+        val params = RetCalcParams(
+            nbOfMonthsInRetirement = 40*12,
+            netIncome = 3000,
+            currentExpenses = 2000,
+            initialCapital = 10000
+        )
+        val actual = RetCalc.determinenbOfMonthsSaving(
+            returns = FixedReturns(0.04),
+            params = params
+        )
+        val expected = 23 * 12 + 1
+        actual === (expected)
+    }
+    
+    it should "not crash if the resulting nbOfMonths is very high" in {
+        val params = RetCalcParams(
+            nbOfMonthsInRetirement = 40*12,
+            netIncome = 3000,
+            currentExpenses = 2999,
+            initialCapital = 0
+        )
+        val actual = RetCalc.determinenbOfMonthsSaving(
+            returns = FixedReturns(0.01),
+            params = params
+        )
+        val expected = 8280
+        actual === (expected)
+    }
+    
+    it should "not loop forever if bad parameters are passed" in {
+        val params = RetCalcParams(
+            nbOfMonthsInRetirement = 40*12,
+            netIncome = 1000,
+            currentExpenses = 2000,
+            initialCapital = 10000
+        )
+        val actual = RetCalc.determinenbOfMonthsSaving(
+            returns = FixedReturns(0.04),
+            params = params
+        )
+        actual === (Int.MaxValue)
     }
 }
--- src/test/scala/returnsTest.scala
+++ src/test/scala/returnsTest.scala
@@ -3,70 +3,59 @@ package retcalc
 
 import org.scalactic.{Equality, TolerantNumerics, TypeCheckedTripleEquals}
 import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
 
 
-class ReturnsSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
-    implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.0001)
+class ReturnsSpec extends AnyFlatSpec {
 
-    "VariableReturns.fromUntil" should {
-        "keep only a window of the return vector" in {
-            val variableReturns = VariableReturns(Vector.tabulate(12)
-                {i => val d = (i+1).toDouble
-                VariableReturn(f"2017.$d%02.0f", d)}
-            )
-            variableReturns.fromUntil("2017.07", "2017.09").returns should === (
-                Vector(VariableReturn("2017.07", 7.0),
-                       VariableReturn("2017.08", 8.0))
-            )
-
-            variableReturns.fromUntil("2017.10", "2018.01").returns should === (
-                Vector(VariableReturn("2017.10", 10.0),
-                       VariableReturn("2017.11", 11.0),
-                       VariableReturn("2017.12", 12.0))
-            )
-        }
-    }
-
-    "Returns.monthlyRate" should {
-        "return a fixed rate for a FixedReturn object" in {
-            Returns.monthlyRate(FixedReturns(0.04), 0) should === (0.04/12)
-            Returns.monthlyRate(FixedReturns(0.04), 10) should === (0.04/12)
-        }
-
-        val variableReturns = VariableReturns(Vector(
-            VariableReturn("2000.01", 0.1),
-            VariableReturn("2000.02", 0.2)))
-        "return the nth rate for VariableReturn" in {
-            Returns.monthlyRate(variableReturns, 0) should === (0.1)
-            Returns.monthlyRate(variableReturns, 1) should === (0.2)
-        }
-
-        "roll over from the first rate if n > length" in {
-            Returns.monthlyRate(variableReturns, 2) should === (0.1)
-            Returns.monthlyRate(variableReturns, 3) should === (0.2)
-            Returns.monthlyRate(variableReturns, 4) should === (0.1)
-        }
-    }
-
-    "Returns.monthlyReturn" should {
-        "return a fixed rate for a FixedReturn" in {
-
-        }
-
-        val variableReturns = VariableReturns(
-            Vector(VariableReturn("2001.01", 0.1), VariableReturn("2001.02", 0.2))
+    "VariableReturns.fromUntil" should "keep only a window of the return vector" in {
+        val variableReturns = VariableReturns(Vector.tabulate(12)
+            {i => val d = (i+1).toDouble
+            VariableReturn(f"2017.$d%02.0f", d)}
+        )
+        variableReturns.fromUntil("2017.07", "2017.09").returns === (
+            Vector(VariableReturn("2017.07", 7.0),
+                    VariableReturn("2017.08", 8.0))
         )
 
-        "return the nth rate for VariableReturn" in {
-        }
+        variableReturns.fromUntil("2017.10", "2018.01").returns === (
+            Vector(VariableReturn("2017.10", 10.0),
+                    VariableReturn("2017.11", 11.0),
+                    VariableReturn("2017.12", 12.0))
+        )
+    }
 
-        "return an error if n > length" in {
-        }
+    "Returns.monthlyRate" should "return a fixed rate for a FixedReturn object" in {
+        Returns.monthlyRate(FixedReturns(0.04), 0) === (0.04/12)
+        Returns.monthlyRate(FixedReturns(0.04), 10) === (0.04/12)
+    }
 
-        "return the n+offset-th rate for OffsetReturn" in {
-            val returns = OffsetReturns(variableReturns, 1)
-            Returns.monthlyRate(returns, 0) should === (0.2)
-        }
+    val variableReturns = VariableReturns(Vector(
+        VariableReturn("2000.01", 0.1),
+        VariableReturn("2000.02", 0.2)))
+    it should "return the nth rate for VariableReturn" in {
+        Returns.monthlyRate(variableReturns, 0) === (0.1)
+        Returns.monthlyRate(variableReturns, 1) === (0.2)
+    }
+
+    it should "roll over from the first rate if n > length" in {
+        Returns.monthlyRate(variableReturns, 2) === (0.1)
+        Returns.monthlyRate(variableReturns, 3) === (0.2)
+        Returns.monthlyRate(variableReturns, 4) === (0.1)
+    }
+
+    "Returns.monthlyReturn" should "return a fixed rate for a FixedReturn" in {
+    }
+
+    it should "return the nth rate for VariableReturn" in {
+    }
+
+    it should "return an error if n > length" in {
+    }
+
+    it should "return the n+offset-th rate for OffsetReturn" in {
+        val returns = OffsetReturns(variableReturns, 1)
+        Returns.monthlyRate(returns, 0) === (0.2)
     }
 
 }
