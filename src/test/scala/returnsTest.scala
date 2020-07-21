502e95e0af7ded1fc89c07c815cacc81ce04a0e1
Henry Tom <henry.tom.sf@gmail.com>
2020-07-20 22:49:06 -0700

    added inflation unit test

--- src/test/scala/returnsTest.scala
+++ src/test/scala/returnsTest.scala
@@ -58,4 +58,25 @@ class ReturnsSpec extends AnyFlatSpec {
         Returns.monthlyRate(returns, 0) === (0.2)
     }
 
+    "Returns.fromEquityAndInflationData" should "compute real total returns from equity and inflation data sources" in {
+        val equities = Vector(
+            EquityData("2117.01", 100.0, 10.0),
+            EquityData("2117.02", 101.0, 12.0),
+            EquityData("2117.03", 102.0, 12.0)
+        )
+
+        val inflations = Vector(
+            InflationData("2117.01", 100.0),
+            InflationData("2117.02", 102.0),
+            InflationData("2117.03", 102.0)
+        )
+
+        val returns = Returns.fromEquityAndInflationData(equities, inflations)
+        returns === (VariableReturns(
+            Vector(
+                VariableReturn("2117.02", (101.0 + 12.0/12) / 100.0 - 102.0/100.0),
+                VariableReturn("2117.03", (102.0 + 12.0/12) / 101.0 - 102.0/102.0)
+            )
+        ))
+    }
 }
