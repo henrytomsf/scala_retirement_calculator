bcb67ca8b48e5d2dd0eaabe9cb5cb1e062ab6c0a
Henry Tom <henry.tom.sf@gmail.com>
2020-08-01 01:05:54 -0700

    IMPORTANT for comprehension note

--- src/main/scala/retcalc.scala
+++ src/main/scala/retcalc.scala
@@ -28,41 +28,55 @@ object RetCalc {
 
     def simulatePlan(returns: Returns,
                      params: RetCalcParams,
-                     nbOfMonthsSaving: Int): (Double, Double) = {
+                     nbOfMonthsSaving: Int): Either[RetCalcError, (Double, Double)] = {
         import params._ //imports all attributes of RetCalcParams into scope to use
         // can also use params.netIncome, etc.
-        val capitalAtRetirement = futureCapital(
-            returns = returns,
-            nbOfMonths = nbOfMonthsSaving,
-            netIncome = netIncome,
-            currentExpenses = currentExpenses,
-            initialCapital = initialCapital
-        )
 
-        val capitalAfterDeath = futureCapital(
-            returns = OffsetReturns(returns, nbOfMonthsSaving),
-            nbOfMonths = nbOfMonthsInRetirement,
-            netIncome = 0,
-            currentExpenses = currentExpenses,
-            initialCapital = capitalAtRetirement
-        )
-        (capitalAtRetirement, capitalAfterDeath)
+        for {
+            capitalAtRetirement <- futureCapital(
+                returns = returns,
+                nbOfMonths = nbOfMonthsSaving,
+                netIncome = netIncome,
+                currentExpenses = currentExpenses,
+                initialCapital = initialCapital
+            )
+            capitalAfterDeath <- futureCapital(
+                returns = OffsetReturns(returns, nbOfMonthsSaving),
+                nbOfMonths = nbOfMonthsInRetirement,
+                netIncome = 0,
+                currentExpenses = currentExpenses,
+                initialCapital = capitalAtRetirement 
+                                    //^ in for comprehension, this will get the value out of the option so it
+                                    //  can be passed into the next function in the for comprehension
+                                    /*
+                                        val q = Right[String, Int](1)
+                                        q.flatMap(foo => Right[String,Int](100))
+                                                foo has been extracted from q as Int
+                                                ex. val x = List(List(Right[String, Int](10), Right[String, Int](20)))
+                                                    x.flatMap(item => item)
+                                                    res: List[scala.util.Right[String,Int]] = List(Right(10), Right(20))
+                                        res: scala.util.Either[String,Int] = Right(100)
+                                    */
+            )
+        } yield (capitalAtRetirement, capitalAfterDeath)
     }
 
     def determinenbOfMonthsSaving(returns: Returns,
                                   params: RetCalcParams): Either[RetCalcError, Int] = {
         import params._
         @tailrec
-        def loop(months: Int): Int = {
-            val (capitalAtRetirement, capitalAfterDeath) = simulatePlan(
-                returns = returns,
-                params = params,
-                nbOfMonthsSaving = months
-            )
-            if (capitalAfterDeath > 0.0) months else loop(months + 1)
+        def loop(months: Int): Either[RetCalcError, Int] = {
+            simulatePlan(returns, params, months) match {
+                case Right((capitalAtRetirement, capitalAfterDeath)) => 
+                    if (capitalAtRetirement > 0.0)
+                        Right(months)
+                    else
+                        loop(months + 1)
+                case Left(err) => Left(err)
+            }
         }
         if (netIncome > currentExpenses) {
-            Right(loop(0))
+            loop(0)
         } else {
             Left(MoreExpensesThanIncome(netIncome, currentExpenses))
         }
