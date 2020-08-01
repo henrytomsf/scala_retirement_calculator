904982722b34b3d827b835d751c59ad931044fd2
Henry Tom <henry.tom.sf@gmail.com>
2020-08-01 00:08:32 -0700

    added ReturnMonthOutOfBounds error

--- src/main/scala/retcalcError.scala
+++ src/main/scala/retcalcError.scala
@@ -3,4 +3,6 @@ package retcalc
 
 sealed abstract class RetCalcError(val message: String)
 case class MoreExpensesThanIncome(income: Double, expenses: Double)
-        extends RetCalcError(s"Expenses: $expenses >= $income!")
+    extends RetCalcError(s"Expenses: $expenses >= $income!")
+case class ReturnMonthOutOfBounds(month: Int, maximum: Int)
+    extends RetCalcError(s"Cannot get return for month $month since out of range. Range is 0 to $maximum")
