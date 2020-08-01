186d2829fe9020c64107020beca5605a869e92dc
Henry Tom <henry.tom.sf@gmail.com>
2020-08-01 00:00:48 -0700

    modified trait

--- src/main/scala/retcalcError.scala
+++ src/main/scala/retcalcError.scala
@@ -2,9 +2,5 @@ package retcalc
 
 
 sealed abstract class RetCalcError(val message: String)
-
-
-object RetCalcError {
-    case class MoreExpensesThanIncome(income: Double, expenses: Double)
+case class MoreExpensesThanIncome(income: Double, expenses: Double)
         extends RetCalcError(s"Expenses: $expenses >= $income!")
-}
